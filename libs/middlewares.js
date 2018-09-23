1d95bd811eaa7b1962856404996e4af3ddcf33c5
Armando Rodriguez <armando.rodriguez@ninjarmm.com>
2018-09-23 11:02:38 -0500

    allows cors on api

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -8,5 +8,11 @@ module.exports = app => {
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
 
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  });
 }
