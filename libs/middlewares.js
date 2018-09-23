70c4d3f8be6dcbb55272bdcfbd0e3dcbe7526b34
Armando Rodriguez <armando.rodriguez@ninjarmm.com>
2018-09-23 13:57:45 -0500

    cors for all main http methods

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -11,6 +11,7 @@ module.exports = app => {
   app.use(morgan('dev'))
 
   app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
