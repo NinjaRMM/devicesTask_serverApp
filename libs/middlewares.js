dc6fe46fd06ace73f03d4b445a3c5416ce7b2382
Yastrenky Bravo <yas.bravo@ninjarmm.com>
2022-04-18 18:12:14 -0400

    Data for insatller endpoint

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -13,7 +13,7 @@ module.exports = app => {
 
   app.use(morgan('dev'))
 
-  app.use(function (req, res, next) {
+  app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
